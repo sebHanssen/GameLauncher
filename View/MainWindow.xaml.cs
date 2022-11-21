747a822ee1490caea059064c6e34e692f7b7bffe
realShirai <sebisch2727@hotmail.com>
2022-11-21 20:38:00 +0100

    Add settings window
    Add settings window and button functionality

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameLauncher.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,15 +18,21 @@ namespace GameLauncher
 {
     public partial class MainWindow : Window
     {
+        SettingsWindow settingsWindow = new SettingsWindow();
         public MainWindow()
         {
             InitializeComponent();
             VideoPlayer.Position = TimeSpan.FromMilliseconds(5000);
         }
-        
-        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+
+            private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
         {
             VideoPlayer.Position = TimeSpan.FromMilliseconds(1);
+        }
+
+        private void SettingsClick(object sender, RoutedEventArgs e)
+        {
+            settingsWindow.Show();
         }
     }
 }
